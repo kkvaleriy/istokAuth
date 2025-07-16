0933f43051804eb07a0e7aafe1217e723d43a9f2
kkvaleriy <kvaleriy126@icloud.com>
2025-07-16 13:25:40 +0300

    feat: added validation for signUp

--- internal/authorization/entities/user.go
+++ internal/authorization/entities/user.go
@@ -29,4 +29,39 @@ type User struct {
 	CreatedAt time.Time
 	UpdatedAt time.Time
 }
+
+func UserForSignUP(dto *dtos.CreateUserRequest) (*User, error) {
+	minLenOfPassword := 8
+
+	if len(strings.TrimSpace(dto.Name)) < 1 {
+		return nil, errors.New("the name of user is required")
+	}
+	if len(strings.TrimSpace(dto.Lastname)) < 1 {
+		return nil, errors.New("the lastname of user is required")
+	}
+	if len(strings.TrimSpace(dto.Nickname)) < 1 {
+		return nil, errors.New("the nickname of user is required")
+	}
+	if len(strings.TrimSpace(dto.Email)) < 1 || !strings.Contains(dto.Email, "@") {
+		return nil, errors.New("the correct email of user is required")
+	}
+	if len(strings.TrimSpace(dto.Password)) < minLenOfPassword {
+		return nil, fmt.Errorf("the password of user must be more than %v characters long", minLenOfPassword)
+	}
+	if dto.Phone < 7_000_000_00_00 {
+		return nil, errors.New("the correct email of user is required")
+	}
+
+	return &User{
+		Name:      dto.Name,
+		Lastname:  dto.Lastname,
+		Nickname:  dto.Nickname,
+		Email:     dto.Email,
+		UserType:  UserTypeUser,
+		IsActive:  true,
+		Phone:     dto.Phone,
+		UUID:      uuid.New(),
+		PassHash:  sha256.Sum256([]byte(dto.Password)),
+		CreatedAt: time.Now(),
+	}, nil
 }
