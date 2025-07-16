9ca59f54c6d605c0df66eab397bf5c4da92c5cbf
kkvaleriy <kvaleriy126@icloud.com>
2025-07-16 21:20:54 +0300

    feat: added usecase for create user

---
+++ internal/authorization/usecase/signup.go
@@ -0,0 +1,41 @@
+package usecase
+
+import (
+	"context"
+
+	"github.com/kkvaleriy/istokAuthorization/internal/authorization/dtos"
+	user "github.com/kkvaleriy/istokAuthorization/internal/authorization/entities"
+)
+
+type UserCreator interface {
+	AddUser(u *user.User) (*user.User, error)
+}
+
+type createUserUseCase struct {
+	repository UserCreator
+}
+
+func NewCreateUser(repository UserCreator) *createUserUseCase {
+	return &createUserUseCase{repository: repository}
+}
+
+func (uc *createUserUseCase) SignUP(_ context.Context, request *dtos.CreateUserRequest) (*dtos.CreateUserResponse, error) {
+
+	userForCreate, err := user.UserForSignUP(request)
+	if err != nil {
+		return nil, err
+	}
+
+	userForCreate, err = uc.repository.AddUser(userForCreate)
+	if err != nil {
+		return nil, err
+	}
+
+	response := &dtos.CreateUserResponse{
+		UUID:      userForCreate.UUID,
+		UserType:  userForCreate.UserType,
+		CreatedAt: userForCreate.CreatedAt,
+	}
+
+	return response, nil
+}
