aa8c91ad30051aba3ac54e1b3d1846ea7585a43e
kkvaleriy <kvaleriy126@icloud.com>
2025-07-21 22:39:48 +0300

    feat: update work with time

--- internal/authorization/repository/postgres/models.go
+++ internal/authorization/repository/postgres/models.go
@@ -1,8 +1,6 @@
 package postgres
 
 import (
-	"time"
-
 	"github.com/google/uuid"
 	user "github.com/kkvaleriy/istokAuthorization/internal/authorization/entities"
 )
@@ -17,7 +15,7 @@ type createUserModel struct {
 	Phone     int       `db:"phone"`
 	UUID      uuid.UUID `db:"UUID"`
 	PassHash  [32]byte  `db:"passHash"`
-	CreatedAt time.Time `db:"createdAt"`
+	CreatedAt string    `db:"createdAt"`
 }
 
 func newCreateUserModel(u *user.User) *createUserModel {
@@ -25,12 +23,12 @@ func newCreateUserModel(u *user.User) *createUserModel {
 		Name:      u.Name,
 		Lastname:  u.Lastname,
 		Nickname:  u.Nickname,
-		Email:     u.Nickname,
+		Email:     u.Email,
 		UserType:  u.UserType,
 		IsActive:  u.IsActive,
 		Phone:     u.Phone,
 		UUID:      u.UUID,
 		PassHash:  u.PassHash,
-		CreatedAt: u.CreatedAt,
+		CreatedAt: u.CreatedAt.Format("20060102'T'15:04:05"),
 	}
 }
