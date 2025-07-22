1c5f4cf7c7f93458c6845aca316207ade54b1b1e
kkvaleriy <kvaleriy126@icloud.com>
2025-07-22 13:50:43 +0300

    feat: refactoring query

--- internal/authorization/repository/postgres/querys/add_user.sql
+++ internal/authorization/repository/postgres/querys/add_user.sql
@@ -9,4 +9,4 @@ INSERT INTO public.users (
     "UUID",
     "passHash",
     "createdAt"
-)VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10);
+)VALUES (@name, @lastname, @nickname, @email, @userType, @isActive, @phone, @UUID, @passHash, @createdAt);
