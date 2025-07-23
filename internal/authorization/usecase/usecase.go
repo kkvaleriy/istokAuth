9320f65fd94d242e9be85a4c51b25a5fddd3389f
kkvaleriy <kvaleriy126@icloud.com>
2025-07-23 16:39:14 +0300

    feat: added logger

--- internal/authorization/usecase/usecase.go
+++ internal/authorization/usecase/usecase.go
@@ -5,14 +5,23 @@ import (
 	"golang.org/x/net/context"
 )
 
+type logger interface {
+	Debug(msg string, args ...any)
+	Info(msg string, args ...any)
+	Warn(msg string, args ...any)
+	Error(msg string, args ...any)
+}
+
 type Repository interface {
 	AddUser(ctx context.Context, u *user.User) error
 }
 
 type usecase struct {
 	repository Repository
+	log        logger
 }
 
-func New(repository Repository) *usecase {
-	return &usecase{repository: repository}
+func New(repository Repository, log logger) *usecase {
+	return &usecase{repository: repository,
+		log: log}
 }
