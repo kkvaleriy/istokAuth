4ee272f67cce3abe31e388ffb041e26551004709
kkvaleriy <kvaleriy126@icloud.com>
2025-07-24 23:10:49 +0300

    feat: added new logger method

--- internal/app/app.go
+++ internal/app/app.go
@@ -20,6 +20,9 @@ type logger interface {
 	Info(msg string, args ...any)
 	Warn(msg string, args ...any)
 	Error(msg string, args ...any)
+	Fatal(msg string, args ...any)
+}
+
 type serverCfg interface {
 	ServerPort() string
 }
