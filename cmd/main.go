7455491008e067d2ea58ba2f59020928d94666ab
kkvaleriy <kvaleriy126@icloud.com>
2025-07-24 23:14:09 +0300

    feat: init app

--- cmd/main.go
+++ cmd/main.go
@@ -11,4 +11,30 @@ import (
 )
 
 func main() {
+	cfg := config.New()
+	log := logger.New(cfg.Logger.Level)
+	server := echo.New()
+	connString := cfg.DataSource.PostgresConnString()
+	dbConfig, err := pgxpool.ParseConfig(connString)
+	if err != nil {
+		log.Error("can't parse connection string", "error", err.Error())
+		panic(err)
+	}
+
+	dbConfig.MaxConns = int32(cfg.DataSource.MaxConnection)
+	dbConfig.MinConns = int32(cfg.DataSource.MinConnection)
+	dbConfig.MaxConnLifetime = cfg.DataSource.LifeTime()
+
+	log.Info("attempting to connect to the database", "connectionString", connString)
+	db, err := pgxpool.NewWithConfig(context.Background(), dbConfig)
+	if err != nil {
+		log.Fatal("the attempt to connect to the database failed", "error", err.Error())
+	}
+	if err := db.Ping(context.Background()); err != nil {
+		log.Fatal("database ping error", "error", err.Error())
+	}
+	log.Info("successful connection to the database")
+
+	app := app.New(db, server, cfg, log)
+	app.Run()
 }
