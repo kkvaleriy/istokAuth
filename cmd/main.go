54a2ab3c25df687a9da735e627297b281d07ee0a
kkvaleriy <kvaleriy126@icloud.com>
2025-07-24 23:13:45 +0300

    feat: init imports

--- cmd/main.go
+++ cmd/main.go
@@ -1,4 +1,14 @@
 package main
 
+import (
+	"context"
+
+	"github.com/jackc/pgx/v5/pgxpool"
+	"github.com/kkvaleriy/istokAuthorization/internal/app"
+	"github.com/kkvaleriy/istokAuthorization/internal/config"
+	"github.com/kkvaleriy/istokAuthorization/pkg/logger"
+	"github.com/labstack/echo/v4"
+)
+
 func main() {
 }
