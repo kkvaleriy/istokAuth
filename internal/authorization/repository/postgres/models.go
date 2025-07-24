9ac8f9b2357929c9f4227e8ff40e79ba3b55b510
kkvaleriy <kvaleriy126@icloud.com>
2025-07-24 23:07:17 +0300

    feat: added new dto with error

--- internal/authorization/repository/postgres/models.go
+++ internal/authorization/repository/postgres/models.go
@@ -1,7 +1,12 @@
 package postgres
 
 import (
+	"fmt"
+	"strconv"
+	"strings"
+
 	"github.com/jackc/pgx/v5"
+	"github.com/kkvaleriy/istokAuthorization/internal/authorization/dtos"
 	user "github.com/kkvaleriy/istokAuthorization/internal/authorization/entities"
 )
 
@@ -17,5 +22,37 @@ func createUserArgs(u *user.User) pgx.NamedArgs {
 		"UUID":      u.UUID,
 		"passHash":  u.PassHash[:],
 		"createdAt": u.CreatedAt,
+	}
+}
+
+func ErrorValidation(constraint string, args pgx.NamedArgs) *dtos.ValidationError {
+	var field, value string
+
+	switch strings.ToLower(constraint) {
+	case "uniq_email":
+		field = "email"
+	case "uniq_nickname":
+		field = "nickname"
+	case "uniq_phone":
+		field = "phone"
+	}
+
+	if len(field) < 1 {
+		return &dtos.ValidationError{
+			Message: "unknown error",
+		}
+	}
+
+	switch args[field].(type) {
+	case int:
+		value = strconv.Itoa(args[field].(int))
+	case string:
+		value = args[field].(string)
+	}
+
+	return &dtos.ValidationError{
+		Message: fmt.Sprintf("a user with the %s %s already exists", field, value),
+		Field:   field,
+		Value:   value,
 	}
 }
