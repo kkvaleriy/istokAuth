3d72456474a3ad97fdd59a946ce30937a7d2c679
kkvaleriy <kvaleriy126@icloud.com>
2025-07-28 14:31:43 +0300

    feat: added swagger endpoint

--- internal/authorization/delivery/http/v1/routes.go
+++ internal/authorization/delivery/http/v1/routes.go
@@ -1,8 +1,15 @@
 package v1
 
-import "github.com/labstack/echo/v4"
+import (
+	_ "github.com/kkvaleriy/istokAuthorization/docs"
+	"github.com/labstack/echo/v4"
+	echoSwagger "github.com/swaggo/echo-swagger"
+)
 
 func (h *handler) Routes(domain *echo.Group) {
+
+	// Swagger endpoint
+	domain.GET("/swagger/*", echoSwagger.WrapHandler)
 
 	domain.POST("/signup", h.signUp)
 }
