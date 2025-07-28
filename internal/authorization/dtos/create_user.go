19b0842de8d9750ef627aaab938c92a34669734a
kkvaleriy <kvaleriy126@icloud.com>
2025-07-28 14:30:09 +0300

    feat: added description for swagger

--- internal/authorization/dtos/create_user.go
+++ internal/authorization/dtos/create_user.go
@@ -6,19 +6,21 @@ import (
 	"github.com/google/uuid"
 )
 
+// @Description User account information for registration
 type CreateUserRequest struct {
-	Name     string `json:"name"`
-	Lastname string `json:"lastname"`
-	Nickname string `json:"nickname"`
-	Email    string `json:"email"`
-	Phone    int    `json:"phone"`
-	Password string `json:"password"`
+	Name     string `json:"name" example:"John"`
+	Lastname string `json:"lastname" example:"Doe"`
+	Nickname string `json:"nickname" example:"Johny1"`
+	Email    string `json:"email" example:"john@email.com"`
+	Phone    int    `json:"phone" example:"79990001122"`
+	Password string `json:"password" example:"mySuperPass"`
 }
 
+// @Description Information about the user's account after successful registration
 type CreateUserResponse struct {
-	UUID      uuid.UUID `json:"uuid"`
-	UserType  string    `json:"user_type"`
-	CreatedAt time.Time `json:"created_at"`
+	UUID      uuid.UUID `json:"uuid" example:"16763be4-6022-406e-a950-fcd5018633ca"`
+	UserType  string    `json:"user_type" example:"USER"`
+	CreatedAt time.Time `json:"created_at" example:"2006-01-02T03:04:05.5141511+03:00"`
 }
 
 type ValidationError struct {
