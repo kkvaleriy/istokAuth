87f9b6457d99eb6b1729d2c3529da2766430de37
kkvaleriy <kvaleriy126@icloud.com>
2025-08-04 18:28:57 +0300

    feat: added example and validation tags

--- internal/auth/dtos/signin_user.go
+++ internal/auth/dtos/signin_user.go
@@ -1,7 +1,7 @@
 package dtos
 
 type SignInRequest struct {
-	Email    string `json:"email"`
-	Phone    int    `json:"phone"`
-	Password string `json:"password"`
+	Email    string `json:"email" example:"john@email.com" validate:"email"`
+	Phone    int    `json:"phone example:"79991112233""`
+	Password string `json:"password example:"mySuperPass" validate:"required,min=8"`
 }
