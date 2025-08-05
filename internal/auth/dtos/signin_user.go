9a00d69e3db5855a016fd7e5d48f5633d94a6daf
kkvaleriy <kvaleriy126@icloud.com>
2025-08-05 21:43:32 +0300

    feat: added swagger tags

--- internal/auth/dtos/signin_user.go
+++ internal/auth/dtos/signin_user.go
@@ -1,11 +1,13 @@
 package dtos
 
+// @Description User credentials.
 type SignInRequest struct {
 	Email    string `json:"email" example:"john@email.com" validate:"email"`
 	Phone    int    `json:"phone example:"79991112233""`
 	Password string `json:"password example:"mySuperPass" validate:"required,min=8"`
 }
 
+// @Description JWT.
 type SignInResponse struct {
 	JWT    string `json:"jwt"`
 	RToken string
