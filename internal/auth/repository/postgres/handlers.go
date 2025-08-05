d34d1b8efb1f7ca20e5ba52de9b1a4b033010ea9
kkvaleriy <kvaleriy126@icloud.com>
2025-08-05 21:34:40 +0300

    feat: update imports

--- internal/auth/repository/postgres/handlers.go
+++ internal/auth/repository/postgres/handlers.go
@@ -3,6 +3,7 @@ package postgres
 import (
 	"context"
 
+	"github.com/jackc/pgx/v5"
 	"github.com/jackc/pgx/v5/pgconn"
 	"github.com/jackc/pgx/v5/pgxpool"
 	user "github.com/kkvaleriy/istokAuth/internal/auth/entities"
