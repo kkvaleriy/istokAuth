3aaec514646665e3bcc581bdec64ca14b1435b36
kkvaleriy <kvaleriy126@icloud.com>
2025-08-05 21:36:12 +0300

    feat: added addrtoken query

--- internal/auth/repository/postgres/querys/querys.go
+++ internal/auth/repository/postgres/querys/querys.go
@@ -5,7 +5,8 @@ import _ "embed"
 var (
 	//go:embed add_user.sql
 	AddUser string
-
+	//go:embed add_rtoken.sql
+	AddRToken string
 	//go:embed check_credentials.sql
 	CheckUserByCredentials string
 )
