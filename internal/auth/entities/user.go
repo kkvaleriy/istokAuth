53a6863f9c077cec59baa77503ed5d8bba77d451
kkvaleriy <kvaleriy126@icloud.com>
2025-08-10 00:21:52 +0300

    feat: made password validation a separate function

--- internal/auth/entities/user.go
+++ internal/auth/entities/user.go
@@ -45,13 +45,12 @@ func SignUp(dto *dtos.CreateUserRequest) (*User, error) {
 	if len(strings.TrimSpace(dto.Email)) < 1 || !strings.Contains(dto.Email, "@") {
 		return nil, errors.New("the correct email of user is required")
 	}
-	if len(strings.TrimSpace(dto.Password)) < minLenOfPassword {
-		return nil, fmt.Errorf("the password of user must be more than %v characters long", minLenOfPassword)
-	}
 	if dto.Phone < 7_000_000_00_00 || dto.Phone > 8_999_999_99_99 {
 		return nil, errors.New("the correct email of user is required")
 	}
-
+	if err := isValidePassword(dto.Password); err != nil {
+		return nil, err
+	}
 	return &User{
 		Name:      dto.Name,
 		Lastname:  dto.Lastname,
@@ -70,8 +69,8 @@ func SignIn(dto *dtos.SignInRequest) (*User, error) {
 	if (len(strings.TrimSpace(dto.Email)) < 1 || !strings.Contains(dto.Email, "@")) && dto.Phone < 7_000_000_00_00 {
 		return nil, errors.New("the correct email or phone number of user is required")
 	}
-	if len(strings.TrimSpace(dto.Password)) < minLenOfPassword {
-		return nil, fmt.Errorf("the password of user can't be empty or less then %v", minLenOfPassword)
+	if err := isValidePassword(dto.Password); err != nil {
+		return nil, err
 	}
 	return &User{Email: dto.Email, Phone: dto.Phone, PassHash: sha256.Sum256([]byte(dto.Password))}, nil
 }
@@ -86,4 +85,11 @@ func (u *User) RefreshToken(ttl time.Duration) *RToken {
 		CreatedAt: issuedAt,
 		ExpiresAt: expiresAt,
 	}
+}
+
+func isValidePassword(pass string) error {
+	if len(strings.TrimSpace(pass)) < minLenOfPassword {
+		return fmt.Errorf("the password of user can't be empty or less then %v", minLenOfPassword)
+	}
+	return nil
 }
