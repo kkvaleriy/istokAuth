4c39e068baa9644417c23131971e4ac66cfda8b5
kkvaleriy <kvaleriy126@icloud.com>
2025-08-09 19:56:32 +0300

    refact: changed names of the field

--- internal/auth/repository/postgres/queries/check_credentials.sql
+++ internal/auth/repository/postgres/queries/check_credentials.sql
@@ -1,3 +1,3 @@
 SELECT  "UUID", nickname, "userType", "isActive"
-	FROM public.users
-	WHERE (phone = @phone or email = @email) and passHash = @passHash;
+	FROM users
+	WHERE (phone = @phone or email = @email) and "passHash" = @passHash;
