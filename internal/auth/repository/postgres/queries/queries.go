ddad75372c2586d8c724db3da0239eb62b0d1d25
kkvaleriy <kvaleriy126@icloud.com>
2025-08-10 00:20:34 +0300

    feat: embed new queries

--- internal/auth/repository/postgres/queries/queries.go
+++ internal/auth/repository/postgres/queries/queries.go
@@ -5,8 +5,14 @@ import _ "embed"
 var (
 	//go:embed add_user.sql
 	AddUser string
+	//go:embed user_type.sql
+	UserType string
+	//go:embed update_user_password.sql
+	UpdateUserPassword string
 	//go:embed add_rtoken.sql
 	AddRToken string
+	//go:embed del_rtoken.sql
+	DelRToken string
 	//go:embed check_credentials.sql
 	CheckUserByCredentials string
 )
