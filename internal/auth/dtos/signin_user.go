3b0863f6f8e8b8552140ec8ab4b84e53b8dd50a1
kkvaleriy <kvaleriy126@icloud.com>
2025-08-10 00:32:37 +0300

    feat: added uuid request

--- internal/auth/dtos/signin_user.go
+++ internal/auth/dtos/signin_user.go
@@ -1,10 +1,14 @@
 package dtos
 
-import "time"
+import (
+	"time"
+
+	"github.com/google/uuid"
+)
 
 // @Description User credentials.
 type SignInRequest struct {
-	Email    string `json:"email" example:"john@email.com" validate:"email"`
+	Email    string `json:"email" example:"john@email.com" `
 	Phone    int    `json:"phone" example:"79991112233"`
 	Password string `json:"password" example:"mySuperPass" validate:"required,min=8"`
 }
@@ -14,6 +18,20 @@ type SignInResponse struct {
 	JWT           string    `json:"jwt"`
 	RToken        string    `json:"-"`
 	ExpiresRToken time.Time `json:"-"`
+}
+
+type UUIDRequest struct {
+	UUID uuid.UUID
+}
+
+func RequestByUUID(UUID string) (*UUIDRequest, error) {
+	u, err := uuid.Parse(UUID)
+	if err != nil {
+		return nil, err
+	}
+	return &UUIDRequest{
+		UUID: u,
+	}, nil
 }
 
 type SignInError struct {
