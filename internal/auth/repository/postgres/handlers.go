6a34125d588eda4f2792af056caa1683cfe656a5
kkvaleriy <kvaleriy126@icloud.com>
2025-08-11 22:07:51 +0300

    feat: added some logs

--- internal/auth/repository/postgres/handlers.go
+++ internal/auth/repository/postgres/handlers.go
@@ -29,13 +29,14 @@ func New(db *pgxpool.Pool, log logger) *repository {
 
 func (r *repository) AddUser(ctx context.Context, u *user.User) error {
 	args := createUserArgs(u)
-	r.log.Debug("the args for query have been created", "args", args)
+	r.log.Debug("The args for query AddUser have been created", "query", queries.AddUser, "args", args)
 
 	return r.insertInDB(ctx, queries.AddUser, args)
 }
 
 func (r *repository) CheckUserByCredentials(ctx context.Context, u *user.User) (*user.User, error) {
 	args := checkUserByCredentialsArgs(u)
+	r.log.Debug("The args for query CheckUserByCredentials have been created", "query", queries.CheckUserByCredentials, "args", args)
 
 	err := r.db.QueryRow(ctx, queries.CheckUserByCredentials, args).Scan(&u.UUID, &u.Nickname, &u.UserType, &u.IsActive)
 	if err != nil {
@@ -47,7 +48,7 @@ func (r *repository) CheckUserByCredentials(ctx context.Context, u *user.User) (
 
 func (r *repository) AddToken(ctx context.Context, t *user.RToken) error {
 	args := addTokenArgs(t)
-	r.log.Debug("the args for query have been created", "args", args)
+	r.log.Debug("The args for query AddToken have been created", "query", queries.AddRToken, "args", args)
 
 	return r.insertInDB(ctx, queries.AddRToken, args)
 
@@ -59,24 +60,23 @@ func (r *repository) insertInDB(ctx context.Context, query string, args pgx.Name
 		return err
 	}
 	defer tx.Rollback(ctx)
-	r.log.Debug("the transaction has started")
+	r.log.Debug("The transaction has started")
 
 	_, err = tx.Exec(ctx, query, args)
 	if err != nil {
-		r.log.Debug("the transaction was failed, rollback")
+		r.log.Error("The transaction was failed, rollback", "Error", err.Error())
 		if pgErr, ok := err.(*pgconn.PgError); ok && pgErr.Code == "23505" {
 			return errorValidation(pgErr.ConstraintName, args)
 		}
 		return err
 	}
 
-	r.log.Debug("the request was successful", "query", query, "args", args)
-
 	err = tx.Commit(ctx)
 	if err != nil {
+		r.log.Error("The transaction commit was failed, rollback", "Error", err.Error())
 		return err
 	}
-	r.log.Debug("the transaction has finished successfuly")
+	r.log.Debug("The transaction has finished successfuly")
 
 	return nil
 }
@@ -87,28 +87,34 @@ func (r *repository) RefreshToken(ctx context.Context, u *user.User, t *user.RTo
 		return nil, err
 	}
 	defer tx.Rollback(ctx)
-	r.log.Debug("the transaction has started")
+	r.log.Debug("The transaction has started")
 
 	err = tx.QueryRow(ctx, queries.DelRToken, t.UUID).Scan(&u.UUID, &u.Nickname)
 	if err != nil {
+		r.log.Error("The transaction was failed, rollback", "Error", err.Error())
 		return nil, err
 	}
 	if len(u.UUID) < 1 {
+		r.log.Warn("Invalid refresh token", "User", u.Nickname)
 		return nil, &dtos.SignInError{Message: "Invalid refresh token"}
 	}
 
 	err = tx.QueryRow(ctx, queries.UserType, u.UUID).Scan(&u.UserType)
 	if err != nil {
+		r.log.Error("The transaction was failed, rollback", "Error", err.Error())
 		return nil, err
 	}
 	if len(u.UserType) < 1 {
+		r.log.Warn("Invalid user", "User", u.Nickname)
 		return nil, &dtos.SignInError{Message: "Invalid user"}
 	}
 
 	err = tx.Commit(ctx)
 	if err != nil {
+		r.log.Error("The transaction commit was failed, rollback", "Error", err.Error())
 		return nil, err
 	}
+	r.log.Debug("The transaction has finished successfuly")
 
 	return u, nil
 }
@@ -120,6 +126,7 @@ func (r *repository) UpdateUserPassword(ctx context.Context, u *user.User) error
 		if pgErr, ok := err.(*pgconn.PgError); ok && pgErr.Code == "23505" {
 			return errorValidation(pgErr.ConstraintName, args)
 		}
+		r.log.Error("User password update error", "Error", err.Error())
 		return err
 	}
 
