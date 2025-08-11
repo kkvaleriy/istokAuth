a17bc2bff8ec931cc8655c373c0b2190f1860045
kkvaleriy <kvaleriy126@icloud.com>
2025-08-11 22:13:47 +0300

    refact: some errors

--- internal/auth/usecase/update_user.go
+++ internal/auth/usecase/update_user.go
@@ -2,6 +2,7 @@ package usecase
 
 import (
 	"context"
+	"fmt"
 
 	"github.com/kkvaleriy/istokAuth/internal/auth/dtos"
 	user "github.com/kkvaleriy/istokAuth/internal/auth/entities"
@@ -10,11 +11,11 @@ import (
 func (uc *userService) UpdateUserPassword(ctx context.Context, request *dtos.UpdateUserPasswordRequest) error {
 	usr, err := user.UpdatePassword(request)
 	if err != nil {
-		return err
+		return fmt.Errorf("update user password: %w", err)
 	}
 	err = uc.repository.UpdateUserPassword(ctx, usr)
 	if err != nil {
-		return err
+		return fmt.Errorf("update user password: %w", err)
 	}
 
 	return nil
